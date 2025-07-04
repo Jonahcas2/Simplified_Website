ba8bf00646cbb968ccef3368bc8995284f4475cf
Jonahcas2 <casablancaazure@gmail.com>
2025-07-04 08:22:20 -0400

    Uploaded Assignment

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function searchVideo() {
+    alert("Search function triggered.");
+}
+
+function showHeroAlert() {
+    alert("Welcome! Here's how to use this site.");
+}
+
+function submitForm() {
+    alert("Video submitted!");
+}
+
+function playVideo(title) {
+    alert("Now playing: " + title);
+}
